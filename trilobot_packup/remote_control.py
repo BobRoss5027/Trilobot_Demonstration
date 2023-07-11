af1028c0d6fb0fd4f60c748addf9f05c08b72634
Eoin <eoinmcneice@rocketmail.com>
2023-07-11 15:35:18 +0100

    Cleaned up remote_control

--- trilobot_packup/remote_control.py
+++ trilobot_packup/remote_control.py
@@ -5,43 +5,14 @@ from sshkeyboard import listen_keyboard
 from distance_stop import wall
 
 tbot = Trilobot()
-global show
-show = True
 
 format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=format, level=logging.DEBUG,datefmt="%H:%M:%S")
-
-def flashing():
-    global show
-    INTERVAL = 0.1  # control the speed of the LED animation
-
-    RED = (255, 0, 0)
-
-    # Map so 0-5 goes from left to right.
-    MAPPING = [LIGHT_REAR_LEFT,
-            LIGHT_MIDDLE_LEFT,
-            LIGHT_FRONT_LEFT,
-            LIGHT_FRONT_RIGHT,
-            LIGHT_MIDDLE_RIGHT,
-            LIGHT_REAR_RIGHT]
-
-    while show:
-        for n in range(0, NUM_UNDERLIGHTS - 1):
-            phy_led = MAPPING[n]
-            tbot.clear_underlighting(show=False)
-            tbot.set_underlight(phy_led, RED)
-            time.sleep(INTERVAL)
-
-        for n in range(NUM_UNDERLIGHTS - 1, 0, -1):
-            phy_led = MAPPING[n]
-            tbot.clear_underlighting(show=False)
-            tbot.set_underlight(phy_led, RED)
-            time.sleep(INTERVAL)
+logging.basicConfig(format=format, level=logging.INFO,datefmt="%H:%M:%S")
             
 GLOBAL_SPEED = 0.8
+GLOBAL_TURN = 0.5
    
-def control(key):  
-    global show   
+def control(key):    
 
     logging.debug("%s pressed", key)
     tbot.clear_underlighting()
@@ -54,14 +25,14 @@ def control(key):
     elif key == "a":
         tbot.set_underlights(LIGHTS_LEFT, r_color=[0,255,0])
         tbot.turn_left(GLOBAL_SPEED)
-        time.sleep(0.5)
+        time.sleep(GLOBAL_TURN)
         tbot.stop()
         tbot.clear_underlighting()
         tbot.set_underlights(LIGHTS_FRONT, r_color=[0,255,0])
     elif key == "d":
         tbot.set_underlights(LIGHTS_RIGHT, r_color=[0,255,0])
         tbot.turn_right(GLOBAL_SPEED)
-        time.sleep(0.5)
+        time.sleep(GLOBAL_TURN)
         tbot.stop()
         tbot.clear_underlighting()
         tbot.set_underlights(LIGHTS_FRONT, r_color=[0,255,0])
@@ -71,5 +42,5 @@ def control(key):
         tbot.fill_underlighting(r_color=[255,0,0])
             
 if __name__ == "__main__":
-    print("Control this robot using w,a,s,d for movement and space to stop")
+    print("Control this robot using w,a,s,d for movement and space to stop. Press ESC to stop this program.")
     listen_keyboard(on_press=control, sequential=True)
