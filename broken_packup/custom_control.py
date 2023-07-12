ef1814e3c82cc33c17cad435baabd3fcbfc6c873
Eoin <eoinmcneice@rocketmail.com>
2023-07-12 22:33:25 +0100

    Update broken control ease of access

--- broken_packup/custom_control.py
+++ broken_packup/custom_control.py
@@ -5,18 +5,21 @@ from sshkeyboard import listen_keyboard
 from distance_stop import wall
 from enum import Enum
 
+# Defines a Trilobot Object to control the motor attachments
 tbot = Trilobot()
 
+# Define logging format and level
+# To show logs, change `level=logging.INFO` -> `level=logging.DEBUG`
 format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=format, level=logging.DEBUG,datefmt="%H:%M:%S")
+logging.basicConfig(format=format, level=logging.INFO,datefmt="%H:%M:%S")
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
             
-GLOBAL_SPEED = 0.8
-TURN_DURATION = 0.25
+GLOBAL_SPEED = 0.8 # Speed to drive at, between 0.0 and 1.0
+GLOBAL_TURN = 0.25 # Time to turn for, in seconds
 
-class RobotState(Enum):
+class RobotState(Enum): # Possible states for the Trilobot to be in
     STOP = 0
     FORWARD = 1
     LEFT = 2
@@ -28,12 +31,12 @@ ROBOTSTATE = RobotState.STOP
 
 class RobotController:
     def __init__(self):
-        self.ROBOTSTATE = RobotState.STOP
+        self.ROBOTSTATE = RobotState.STOP   # Import Trilobot States
     def control(self,key): 
         logging.debug("%s pressed", key)
         tbot.clear_underlighting()
         if key == "w":
-            if self.ROBOTSTATE == RobotState.BACKWARD:
+            if self.ROBOTSTATE == RobotState.BACKWARD:  # If Trilobot is already moveing backwards, stop
                 tbot.stop()
                 self.ROBOTSTATE = RobotState.STOP
             else:
@@ -49,11 +52,13 @@ class RobotController:
         elif key == "a":
             #Program the robot to turn left when the a key is pressed
             #If the robot is currently turning right then stop the robot
+            tbot.set_underlights(LIGHTS_LEFT, r_color=GREEN)
             tbot.turn_left(GLOBAL_SPEED)
         elif key == "d":
             #Program the robot to turn right when the d key is pressed
             #If the robot is currently turning left then stop the robot
             tbot.set_underlights(LIGHTS_RIGHT, r_color=GREEN)
+            tbot.turn_right(GLOBAL_SPEED)
         elif key == "q":
             #If the q key is pressed make the robot dance!!
             tbot.set_underlights(LIGHTS_FRONT, r_color=RED)
