05e5b6dfe1f434fa823df3344f21064a640fb415
Eoin <eoinmcneice@rocketmail.com>
2023-07-12 22:16:17 +0100

    Added documentation

--- broken_packup/remote_control.py
+++ broken_packup/remote_control.py
@@ -4,43 +4,52 @@ import logging
 from sshkeyboard import listen_keyboard
 from distance_stop import wall
 
+"""
+Simple program to control a Trilobot using an SSH connection.
+
+Control the Trilobot by using W,A,S,D to move and turn, and press space to stop
+
+Press ESC to end the program
+"""
+
+# Defines a Trilobot Object to control the motor attachments
 tbot = Trilobot()
 
+# Define logging format and level
+# To show logs, change `level=logging.INFO` -> `level=logging.DEBUG`
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO,datefmt="%H:%M:%S")
-            
-GLOBAL_SPEED = 0.8
-GLOBAL_TURN = 0.5
+          
+GLOBAL_SPEED = 0.8 # Speed to drive at, between 0.0 and 1.0
+GLOBAL_TURN = 0.5 # Time to turn for, in seconds
    
 def control(key):    
-
     logging.debug("%s pressed", key)
     tbot.clear_underlighting()
-    if key == "w":
+    if key == "w":  # Move forwards when w pressed
         tbot.set_underlights(LIGHTS_FRONT, r_color=[0,255,0])
         tbot.forward(GLOBAL_SPEED)
-    elif key == "s":
+    elif key == "s":    # Move backwards when s pressed
         tbot.set_underlights(LIGHTS_REAR, r_color=[0,255,0])
         tbot.backward(GLOBAL_SPEED)
-    elif key == "a":
+    elif key == "a":    # Turn left when a pressed
         tbot.set_underlights(LIGHTS_LEFT, r_color=[0,255,0])
         tbot.turn_left(GLOBAL_SPEED)
         time.sleep(GLOBAL_TURN)
         tbot.stop()
         tbot.clear_underlighting()
         tbot.fill_underlighting(r_color=[0,0,255])
-    elif key == "d":
+    elif key == "d":    # Turn right when d pressed
         tbot.set_underlights(LIGHTS_RIGHT, r_color=[0,255,0])
         tbot.turn_right(GLOBAL_SPEED)
         time.sleep(GLOBAL_TURN)
         tbot.stop()
         tbot.clear_underlighting()
         tbot.fill_underlighting(r_color=[0,0,255])
-    else:
+    else:   # Stop when any other key pressed 
         tbot.stop()
-        show=True
         tbot.fill_underlighting(r_color=[255,0,0])
             
 if __name__ == "__main__":
     print("Control this robot using w,a,s,d for movement and space to stop. Press ESC to stop this program.")
-    listen_keyboard(on_press=control, sequential=True)
+    listen_keyboard(on_press=control, sequential=True)  # Listens for key pressed in SSH sessions
