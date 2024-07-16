132c69ff2764debfa7a61ec540f0347865fca89e
Eoin <45468358+BobRoss5027@users.noreply.github.com>
2024-07-16 10:32:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -154,7 +154,7 @@ cat << EOF > $UNINSTALLER
 printf "It's recommended you run these steps manually.\n"
 printf "If you want to run the full script, open it in\n"
 printf "an editor and remove 'exit 1' from below.\n"
-exit 1
+
 EOF
 
 printf "$LIBRARY_NAME $LIBRARY_VERSION Python Library: Installer\n\n"
